b237594c9eaedd53f08fc053b8f96d4b1b911d67
karen <klisse22@jaumebalmes.net>
2023-01-16 20:41:25 +0100

    ejercicios

---
+++ B-9/conc.py
@@ -0,0 +1,9 @@
+#Imprimir el resultat de la suma de les dues variables (int)
+x,y = (2,8)
+z = x+y
+print(z)
+
+#Imprimir el resultat de la suma de les dues variables (int) dintre d'un text (string)
+x,y = (2,2)
+z = x+y
+print('Hola persona, tinc ' + str(z) + ' gats')
